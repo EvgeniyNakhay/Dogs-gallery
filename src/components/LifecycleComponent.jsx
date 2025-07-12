bff106e10558365c63f913299c9b8e91a459ad63
EvgeniyNakhay <qwert57894@gmail.com>
2025-07-12 18:08:11 +0300

    added select

--- src/components/LifecycleComponent.jsx
+++ src/components/LifecycleComponent.jsx
@@ -7,6 +7,8 @@ export default function LifecycleComponent() {
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
   const [inputValue, setInputValue] = useState(3)
+  const [allBreedsList, setAllBreedsList] = useState(null)
+  const [selectedBreed, setSelectedBreed] = useState('')
 
   async function fetchImages() {
     try {
@@ -26,6 +28,38 @@ export default function LifecycleComponent() {
     }
   }
 
+  async function getAllBreeds() {
+    try {
+      const response = await fetch(`https://dog.ceo/api/breeds/list/all`)
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`)
+      }
+      const data = await response.json()
+      setAllBreedsList(Object.keys(data.message))
+    } catch (e) {
+      setError(e)
+      console.error('Ошибка при загрузке всех пород собак:', e)
+    }
+  }
+
+  async function fetchImagesbySelectedBreed() {
+    try {
+      const response = await fetch(
+        `https://dog.ceo/api/${selectedBreed}/image/random/${inputValue}`
+      )
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`)
+      }
+      const data = await response.json()
+      setImages(data.message)
+      setLoading(false)
+    } catch (e) {
+      setError(e)
+      setLoading(false)
+      console.error('Ошибка при загрузке выбранной породы собак:', e)
+    }
+  }
+
   function handleInputChange(event) {
     setInputValue(event.target.value)
   }
@@ -35,9 +69,18 @@ export default function LifecycleComponent() {
     setRestoresCount((restoresCount) => restoresCount + 1)
   }
 
+  function selectBreed(event) {
+    setSelectedBreed(event.target.value)
+  }
+
   useEffect(() => {
     fetchImages()
+    getAllBreeds()
   }, [])
+
+  //   useEffect(() => {
+  //     fetchImagesbySelectedBreed()
+  //   }, [selectedBreed])
 
   if (loading) {
     return <p>Loading...</p>
@@ -51,6 +94,15 @@ export default function LifecycleComponent() {
     <div>
       <h1>Галерея собак</h1>
       <h2>Картинки обновлены {restoresCount} раз(а)</h2>
+      <label>
+        Порода:
+        <select value={selectedBreed} onChange={selectBreed}>
+          <option disabled>Выберите...</option>
+          {allBreedsList.map((item) => (
+            <option value={item}>{item}</option>
+          ))}
+        </select>
+      </label>
       <div style={{ marginBottom: 20 }}>
         <label>Показать </label>
         <input
